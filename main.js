befb8536ee16c782b56ad690fb39e21711e10fe4
yaram <yaramodesu@gmail.com>
2021-04-22 13:20:11 +0300

    toimii välttävästi

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 
-
 let edellinenLukuInput = document.querySelector('#edellinenluku')
 let nykyinenLukuInput = document.querySelector('#nykyinenluku')
 
@@ -8,27 +7,54 @@ const operaatiot = document.querySelectorAll('.operaatio')
 const tulosNappi = document.querySelector('.summa')
 const korjaa = document.querySelector('.korjaa')
 const poistaKaikki = document.querySelector('.poistakaikki')
-const piste = document.querySelector('.piste')
+const summa = document.querySelector('.summa')
 
 let nykyinenLuku = ''
 let edellinenLuku = ''
 let operaatio = ''
 
+/* eventlistenerit */
 numerot.forEach(nappi => {
     nappi.addEventListener('click', () => {
-        lisääNumero(nappi.innerText)
-        console.log(nappi.innerText)
+        if(operaatio === 'summa') {
+            edellinenLuku = ''
+            päivitä()
+        }
+        if(nappi.innerText === '.' && nykyinenLuku.includes('.')) {
+            return
+        }
+        lisääNumero(nappi.innerText)       
     })
 
 })
 operaatiot.forEach(nappi => {
     nappi.addEventListener('click', () => {
-        valitseOperaatio(nappi.innerText)
-       
+        valitseOperaatio(nappi.innerText)     
     })
-
 })
 
+poistaKaikki.addEventListener('click', () => {
+    nykyinenLuku = ''
+    edellinenLuku = ''
+    operaatio = ''
+    päivitä()
+})
+korjaa.addEventListener('click', () => {
+    const leikkaa = nykyinenLuku.slice(0,-1)
+    nykyinenLuku = leikkaa
+    päivitä()
+})
+summa.addEventListener('click', ()=> {
+    if(edellinenLuku !== '' && nykyinenLuku !== '') {
+        edellinenLuku = laske()
+        nykyinenLuku = ''
+        operaatio = 'summa'
+        päivitä()
+    }
+})
+
+
+/* funktiot */
 function päivitä() {
     nykyinenLukuInput.value = nykyinenLuku
     edellinenLukuInput.value =edellinenLuku
@@ -40,9 +66,33 @@ function lisääNumero(numero)  {
 }
 
 function valitseOperaatio(toimitus){
-    console.log(toimitus)
+    if(nykyinenLuku === ''){
+        return
+    }
+    if(edellinenLuku !== ''){
+        nykyinenLuku = laske()
+        päivitä()
+        operaatio = toimitus
+    }
     operaatio = toimitus
     edellinenLuku = nykyinenLuku
     nykyinenLuku = ''
     päivitä()
+}
+
+function laske () {
+    num1 = parseFloat(edellinenLuku)
+    num2 = parseFloat(nykyinenLuku)
+    if(operaatio === "+") {
+        return num1+num2
+    }
+    else if(operaatio === "-") {
+        return num1-num2
+    }
+    else if(operaatio === "*") {
+        return num1*num2
+    }
+    else if(operaatio === "/") {
+        return num1/num2
+    }
 }
